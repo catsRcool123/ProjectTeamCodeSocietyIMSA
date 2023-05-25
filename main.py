31699f77a2a1f96949cd3dd62e0ca0d9a296a1dc
cool cats <102429275+catsRcool123@users.noreply.github.com>
2023-05-25 06:45:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,14 @@ from funny_image import WordCloudGenerator
 
 app = Flask(__name__)
 
+@app.route('/git_update', methods=['POST'])
+def git_update():
+    repo = git.Repo('./orbe')
+    origin = repo.remotes.origin
+    repo.create_head('main',
+                     origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return '', 200
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
